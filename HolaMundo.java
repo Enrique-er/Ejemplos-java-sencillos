f81d93f876ea44936c0f8d2c34c374f310a2c4d4
Enrique Embil Rodriguez <enriqueer78@educastur.es>
2023-11-19 23:14:08 +0100

    Añadimos nuestro primeros programas JAVA

---
+++ HolaMundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tarde
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hola Mundo");
+	}
+
+ 
+}
